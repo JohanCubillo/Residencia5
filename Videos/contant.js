3c2049044f81482d51410beea6306de9c52f5662
Johan Cubillo <jcusa@aol.com>
2021-11-18 07:38:08 -0600

    Agregue La carpeta Videos -Johan

---
+++ Videos/contant.js
@@ -0,0 +1,18 @@
+
+(function (){
+    'use strict'
+
+
+var timer = setTimeout(() => {
+  
+
+  const caja =document.querySelector('#container').style.display="block"
+
+}, 5000)
+})();
+
+  
+
+
+
+
