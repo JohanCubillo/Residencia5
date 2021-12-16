86289c930520a123347062adb64f7f60215dbb53
Johan Cubillo <jcusa@aol.com>
2021-12-16 06:51:30 -0600

    Se corrije un error en js

--- Videos/menu_lateral.js
+++ Videos/menu_lateral.js
@@ -1,32 +1,9 @@
-// <!-- Script importante para que funcione el menu lateral -->
+const btn = document.querySelector('#menu-btn');
+const menu = document.querySelector('#side-menu');
 
-
-    const button = document.querySelector(".video");
-    const close = document.querySelector(".close");
-    const trailer = document.querySelector(".trailer");
-    const video = document.querySelector(".didier");
-
-
-
-
-    button.addEventListener('click', () => {
-        video.play();
-        trailer.style.visibility = "visible";
-        trailer.style.opacity = 1;
-
-
-
-
-    });
-
-    close.addEventListener('click', () => {
-        trailer.style.visibility = "hidden";
-        trailer.style.opacity = 0;
-        video.pause();
-        video.currentTime = 0;
-    });
-
-  
-
-
+btn.addEventListener('click', e => {
+    menu.classList.toggle("menu-collapsed");
+    menu.classList.toggle("menu-expanded");
+    document.querySelector('body').classList.toggle('body-expanded');
+});
 
